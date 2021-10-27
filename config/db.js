b6836c356c1e94c6da359edfb3f55747dbb3347e
KSarra99 <khmilasarra@gmail.com>
2021-10-27 22:07:51 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect( 'mongodb://localhost:27017/mern' ,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
